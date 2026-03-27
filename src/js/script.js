c1574dea53e2a875c957c195faa8fe88f876dba4
Michel <michel.hanada@icloud.com>
2026-03-27 09:50:41 -0300

    feat: declaracoes e variaveis

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+// DECLARAÇÕES E VARIAVEIS
+//VAR, LET CONST
+
+var nome="Fiap";
+console.log(nome);
+var nome="Dev"
+console.log(nome);
+
+if(true){
+    var avatar="Fiapinho";
+}
+
+console.log(avatar);
